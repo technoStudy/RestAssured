d67ad0081b6856884771384cf4860e53d0eb3e39
TechnoStudy <info@techno.study>
2020-02-18 20:29:31 -0500

    query parameters

--- src/test/java/IntroductionTestsSolution.java
+++ src/test/java/IntroductionTestsSolution.java
@@ -99,4 +99,20 @@ public class IntroductionTestsSolution {
                 log().status().
                 statusCode( 200 );
     }
+
+    //https://gorest.co.in/public-api/users?_format=json&access-token=j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap
+    @Test
+    public void queryParamsTest(){
+        given().
+                log().uri().
+                param( "access-token", "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ).
+                param( "_format", "json" ).
+                when().
+                get("https://gorest.co.in/public-api/users").
+                then().
+                log().status().
+                log().body().
+                statusCode( 200 );
+    }
+
 }
