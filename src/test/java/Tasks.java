bbc27b3f2ff661d681caf5e1587800c4b0fca89c
TechnoStudy <info@techno.study>
2020-02-18 21:00:09 -0500

    Tasks

---
+++ src/test/java/Tasks.java
@@ -0,0 +1,9 @@
+public class Tasks {
+
+    /**
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type text
+     * expect body to be equal "203 Non-Authoritative Information"
+     * **/
+}
