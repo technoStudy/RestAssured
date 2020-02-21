fcf876908f14c605fc0dd0daf720ee483a59f0b1
TechnoStudy <info@techno.study>
2020-02-20 21:50:01 -0500

    GoRest Create and Delete User

--- src/test/java/GoRestTest.java
+++ src/test/java/GoRestTest.java
@@ -1,8 +1,8 @@
-import org.junit.Before;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+import pojo.GoRestUser;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 
@@ -11,8 +11,8 @@ public class GoRestTest {
     @Test
     public void queryParamsTest(){
         given().
-                log().uri().
-                param( "access-token", "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ).
+                log().uri()
+                .param( "access-token", "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ).
                 param( "_format", "json" ).
                 when().
                 get("https://gorest.co.in/public-api/users").
@@ -61,5 +61,37 @@ public class GoRestTest {
                 log().status().
                 log().body().
                 body( "_meta.code", equalTo( 401 ) );
+    }
+
+    @Test
+    public void createUserTest() {
+        GoRestUser user = new GoRestUser();
+        user.setEmail( "as2fa123sdf@asd.as" );
+        user.setFirstName( "My First Name" );
+        user.setLastName( "My Last Name" );
+        user.setGender( "male" );
+
+
+        String userId = given()
+                .contentType( ContentType.JSON )
+                .auth()
+                .oauth2( "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ) // basic OAuth 2
+                .body( user )
+                .when()
+                .post( "https://gorest.co.in/public-api/users" )
+                .then()
+//                .log().body()
+                .body( "_meta.code", equalTo( 201 ) )
+                .extract().jsonPath().getString( "result.id" );
+
+        given()
+                .auth()
+                .oauth2( "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ) // basic OAuth 2
+                .when()
+                .delete("https://gorest.co.in/public-api/users/"+userId)
+                .then()
+//                .log().all()
+                .body( "_meta.code", equalTo( 204 ) )
+        ;
     }
 }
---
+++ src/test/java/pojo/GoRestUser.java
@@ -0,0 +1,47 @@
+package pojo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GoRestUser {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @JsonProperty("first_name")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonProperty("last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    @JsonProperty("last_name")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
