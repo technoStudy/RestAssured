5a6c18ced8791d0aed4520009e70e3e60e0b18df
TechnoStudy <info@techno.study>
2020-02-20 20:36:40 -0500

    Simple Query Parameter auth

--- src/test/java/GoRestTest.java
+++ src/test/java/GoRestTest.java
@@ -1,4 +1,25 @@
 import org.junit.Before;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class GoRestTest {
+    //https://gorest.co.in/public-api/users?_format=json&access-token=j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap
+    @Test
+    public void queryParamsTest(){
+        given().
+                log().uri().
+                param( "access-token", "j6XoJSutZrv-ikB-4X4_Zndi54_iqSZES-Ap" ).
+                param( "_format", "json" ).
+                when().
+                get("https://gorest.co.in/public-api/users").
+                then().
+                log().status().
+                log().body().
+                body( "_meta.code", equalTo( 200 ) );
+    }
+
 }
