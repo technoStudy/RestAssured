54b7e0e2073ad26f178b164e4b7091aaa4964d15
TechnoStudy <info@techno.study>
2020-02-29 14:33:21 -0500

    Review Task 3 Solution

--- src/test/java/review/ReviewTasksSolution.java
+++ src/test/java/review/ReviewTasksSolution.java
@@ -3,6 +3,7 @@ package review;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -34,5 +35,26 @@ public class ReviewTasksSolution {
                 .then()
                 .body( "places", not( empty()) )
                 ;
+    }
+
+    @Test(dataProvider = "task3data")
+    public void task3(String placeName, String country, String zipCode){
+        given()
+                .pathParam( "country", country )
+                .pathParam( "zipcode", zipCode )
+                .when()
+                .get("/{country}/{zipcode}")
+                .then()
+                .body( "places.'place name'", hasItem( placeName ) )
+        ;
+    }
+
+    @DataProvider
+    public Object[][] task3data() {
+        return new Object[][] {
+                {"Beverly Hills", "us", "90210"},
+                {"Fort-de-France", "MQ", "97200"},
+                {"Çiçekli Köyü", "TR", "01000"},
+        };
     }
 }
