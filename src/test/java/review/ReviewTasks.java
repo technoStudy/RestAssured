02e3064dfa81b3267e2beee5a9f554e95a0646d5
TechnoStudy <info@techno.study>
2020-02-29 12:37:15 -0500

    Review Task 2

--- src/test/java/review/ReviewTasks.java
+++ src/test/java/review/ReviewTasks.java
@@ -4,5 +4,11 @@ public class ReviewTasks {
     /** Task 1
      * create a get request to zippo api us/90210
      * validate that first place's place name is Beverly Hills
-     * */
+     **/
+
+    /** Task 2
+     * create a get request to zippo api {country}/{zipcode}
+     * provide any country and zipcode
+     * validate that first places array is not empty
+     **/
 }
