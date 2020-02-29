8a00aedf372659bfcdefb62ceedddc1b39b26dd8
TechnoStudy <info@techno.study>
2020-02-29 12:31:47 -0500

    Review Task 1 Solution

---
+++ src/test/java/review/ReviewTasksSolution.java
@@ -0,0 +1,25 @@
+package review;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ReviewTasksSolution {
+    @BeforeClass
+    public void init(){
+        RestAssured.baseURI = "http://api.zippopotam.us";
+    }
+
+    @Test
+    public  void task1() {
+        given()
+                .when()
+                .get("us/90210")
+                .then()
+        .body( "places[0].'place name'", equalTo( "Beverly Hills" ) )
+                ;
+    }
+}
